d8590edb3d25ac6c5557d0a6f3a764c9f8081d32
Hemanth Gowda2005 <yashayashhemu@gmail.com>
2026-03-31 18:15:02 +0530

    object class

---
+++ Wire.java
@@ -0,0 +1,13 @@
+class Wire {
+    String material;
+    int length;
+    double thickness;
+    char grade;
+    boolean isInsulated;
+    long price;
+
+    public Wire() {
+        System.out.println("Default Constructor Called");
+        System.out.println("Wire object created");
+    }
+    }
